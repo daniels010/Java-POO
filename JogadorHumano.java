8b4b518914efbe215482453947518cb7581e3a10
daniels010 <114633244+daniels010@users.noreply.github.com>
2024-01-18 14:05:15 -0300

    Add files via upload

---
+++ JogadorHumano.java
@@ -0,0 +1,19 @@
+//package atividade02;
+
+public class JogadorHumano {
+    private char simbolo;
+
+    public JogadorHumano(char simbolo){
+        this.simbolo = simbolo;
+    }
+
+    public char obterSimbolo() {
+        return simbolo;
+    }
+    public int obterLinha(){
+        return obterLinha();
+    }
+    public int obterColuna(){
+        return obterColuna();
+    }
+}
